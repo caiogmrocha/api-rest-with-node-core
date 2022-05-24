119ba23e89f026939b11f0c1176e22519ef9306a
caiogmrocha <caioguilherme739@gmail.com>
2022-05-23 21:33:37 -0300

    feat: add GetAllStudentsFactory.handle to router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,12 @@
+const GetAllStudentsFactory = require('../factories/get-all-students');
+const getAllStudentsController = GetAllStudentsFactory.getInstance();
+
 const DEFAULT_HEADER = {
   'Content-Type': 'application/json',
 };
 
 const routes = {
-  "/students:get": (request, response) => {
-    const students = [
-      {
-        id: 1,
-        name: 'William',
-        email: 'william@email.com',
-        country: 'Brazil',
-        course: 'Psychology',
-      },
-    ];
-
-    response.writeHead(200, DEFAULT_HEADER);
-    response.write(JSON.stringify(students));
-
-    return response.end();
-  },
+  "/students:get": getAllStudentsController.handle,
   "notFound": () => {
     response.writeHead(404, DEFAULT_HEADER);
     response.write(JSON.stringify({ message: 'Página não encontrada.' }));
@@ -37,4 +25,4 @@ const httpHandler = (request, response) => {
   return chosenRoute(request, response);
 };
 
-module.exports = { routes, httpHandler };
+module.exports = { routes, httpHandler, DEFAULT_HEADER };
